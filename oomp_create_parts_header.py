4d2ff1aa0cb0e32c04f13f6040da2194e61b42de
Oom Lout <aaron@oomlout.com>
2023-08-18 17:04:45 +0100

    comitting after 1 generations

--- oomp_create_parts_header.py
+++ oomp_create_parts_header.py
@@ -45,7 +45,27 @@ def load_parts(**kwargs):
     parts.append(part_details)
 
 
-
+    #2 row headers
+    #headers up to 40 pin
+    #define a part 
+    part_details = {}
+    part_details["classification"] = "electronic"
+    part_details["type"] = "header"
+    part_details["size"] = ["2d54_mm_dual_row"]
+    part_details["color"] = [""]
+    part_details["description_main"] = []
+    # add 1- 40 _pin
+    for pin_count in range(2, 40, 2):
+        part_details["description_main"].append(f"2x{pin_count/2}_dual_row_{pin_count}_pin")
+    #part_details["description_extra"] = ["right_angle","surface_mount","","surface_mount_right_angle"]
+    part_details["description_extra"] = [""]
+    part_details["manufacturer"] = ""
+    part_details["part_number"] = ""
+    part_details["kicad_reference"] = "J"
+    #add the part to the list of parts
+    parts.append(part_details)
     
+
+
     oomp.add_parts(parts, make_files=make_files)
     
