5d6a1bc01e84883ff696b855c88b778e7e892089
Oom Lout <aaron@oomlout.com>
2023-11-13 22:19:17 +0000

    labels

--- action_setup.py
+++ action_setup.py
@@ -28,7 +28,7 @@ def main():
 
     #filter is the type to import
     filter = ""
-    filter = "connector"
+    #filter = "connector"
     oomp.load_parts(from_yaml=False, make_files=True, filter=filter)
     oomp.save_parts()
 
